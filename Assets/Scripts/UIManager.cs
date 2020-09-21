118dfe252f96ffd5f81b320347ee4860af6c5531
Masticater <30707218+Masticater@users.noreply.github.com>
2020-09-20 23:20:16 -0400

    Started Inventory Control
    -Updated UI
    --Resized icons
    --Added all icon slots
    -Created base code for inventory selection within the UI
    -Erased "GitHubCopy" within Unity so it doesn't show again.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,10 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     UIManager Instance;
+    [SerializeField]
+    RectTransform selector;
+    [SerializeField]
+    RectTransform selection;
+
+    [Header("Inventory Slots")]
+    public GameObject[] left;
+    public GameObject[] up, right, down;
+    Vector2 inventoryDirection;
 
     void Awake()
     {
@@ -21,11 +31,40 @@ public class UIManager : MonoBehaviour
 
     private void Start()
     {
-        
+        UpdateUI();
+    }
+
+    void UpdateUI()
+    {
+        selector.position = selection.position;
     }
 
     void Update()
     {
+        if (Input.GetButtonDown("Inventory_Horizontal") || Input.GetButtonDown("Inventory_Vertical"))
+        {
+            inventoryDirection = new Vector2(Input.GetAxisRaw("Inventory_Horizontal"), Input.GetAxisRaw("Inventory_Vertical"));
+
+            if(inventoryDirection == Vector2.left)
+            {
+                print("Left arrow pressed");
+            }
+            else if(inventoryDirection == Vector2.up)
+            {
+                print("Up arrow pressed");
+            }
+            else if (inventoryDirection == Vector2.right)
+            {
+                print("Right arrow pressed");
+            }
+            else if (inventoryDirection == Vector2.down)
+            {
+                print("Down arrow pressed");
+            }
+                print("Inventory button pushed");
+        }
+        else
+            inventoryDirection = Vector2.zero;
         
     }
 }
